d2a260accf02d243e3a0f806e49122a06fff269a
Corey Psoinos <coreypsoinos@gmail.com>
2022-04-30 19:58:38 -0400

    sizing for mobile layout

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex items-center justify-center min-h-screen">
+    <div
+      class="max-w-[390px] w-full flex mx-auto flex-col relative shadow-2xl overflow-hidden overflow-y-scroll sm:rounded-xl"
+    ></div>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: 'antialiased bg-gray-900'
+  }
+})
+</script>
