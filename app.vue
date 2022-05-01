24c80d0809554b5dd718a05e61b0759ba1ca3517
Corey Psoinos <coreypsoinos@gmail.com>
2022-04-30 20:05:37 -0400

    lint

--- app.vue
+++ app.vue
@@ -1,11 +1,3 @@
-<template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div
-      class="max-w-[390px] w-full flex mx-auto flex-col relative shadow-2xl overflow-hidden overflow-y-scroll sm:rounded-xl"
-    ></div>
-  </div>
-</template>
-
 <script setup lang="ts">
 useHead({
   bodyAttrs: {
@@ -13,3 +5,11 @@ useHead({
   }
 })
 </script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div
+      class="max-w-[390px] w-full flex mx-auto flex-col relative shadow-2xl overflow-hidden overflow-y-scroll sm:rounded-xl"
+    ></div>
+  </div>
+</template>
