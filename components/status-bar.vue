eca3868210695afcbc994f39aac4e32f3645db09
Corey Psoinos <coreypsoinos@gmail.com>
2022-05-01 21:03:55 -0400

    set svgo options and add icons to component

--- components/status-bar.vue
+++ components/status-bar.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
 import IconWifi from '~/assets/svg/ios/wifi.svg'
+import IconCellularConnectivity from '~/assets/svg/ios/cellular-connectivity.svg'
+import IconBattery from '~/assets/svg/ios/battery.svg'
+
+const now = ref(new Date())
+
+const time = computed(() => {
+  return new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  })
+    .format(now.value)
+    .split(' ')[0]
+})
 </script>
 
 <template>
-  <div class="pt-6 mb-8 text-white font-apple">
-    <span>10:57</span>
-    <icon-wifi class="text-white" />
+  <div
+    class="flex pt-6 mb-8 text-white font-apple items-center justify-between w-full"
+  >
+    <span class="font-semibold">{{ time }}</span>
+    <div class="flex items-center gap-2">
+      <icon-cellular-connectivity class="text-white w-5" />
+      <icon-wifi class="block text-white w-5 h-5" />
+      <icon-battery class="text-white h-3" />
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@coreyander/nuxt-svgo'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    ['@coreyander/nuxt-svgo', { multipass: true, removeViewBox: false }]
+  ],
   typescript: {
     shim: false
   }
