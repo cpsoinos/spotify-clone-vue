ec0987953fdc43a67889303b753a4963613dba82
Corey Psoinos <coreypsoinos@gmail.com>
2022-05-02 19:55:53 -0400

    add border and page slot

--- app.vue
+++ app.vue
@@ -9,10 +9,11 @@ useHead({
 <template>
   <div class="flex justify-center items-center min-h-screen">
     <div
-      class="flex overflow-hidden overflow-y-scroll relative flex-col mx-auto w-full max-w-[390px] h-[843px] shadow-2xl sm:rounded-xl"
+      class="now-playing flex overflow-hidden overflow-y-scroll relative mx-auto w-full max-w-[390px] h-[843px] shadow-2xl rounded-[3rem] border-2 border-zinc-600 bg-black"
     >
       <div class="flex flex-col items-center px-6 w-full">
         <status-bar />
+        <nuxt-page />
       </div>
     </div>
   </div>
