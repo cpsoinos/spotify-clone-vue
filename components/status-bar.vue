b8b6e0663462bc654581a9b977bcf356a0b63f98
Corey Psoinos <coreypsoinos@gmail.com>
2022-05-01 20:19:39 -0400

    use @coreyander/nuxt-svgo

--- components/status-bar.vue
+++ components/status-bar.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IconWifi from '~/assets/svg/ios/wifi.svg'
+</script>
 
 <template>
   <div class="pt-6 mb-8 text-white font-apple">
     <span>10:57</span>
+    <icon-wifi class="text-white" />
   </div>
 </template>
